af021a42ff83df827b80a8b37bd5ff79d07b0b0e
Balou9 <emmanuel.owosekun@gmail.com>
2021-09-06 07:25:30 +0200

    chore: adds test comments

--- extermitators-api/test.js
+++ extermitators-api/test.js
@@ -6,14 +6,14 @@ const sampleRequestObject3 = [ "001", "002", "001", "001", "002", "001", "002"]
 
 tape("Calculates the total cost", (t) => {
   const totalCost1 = calcTotalPrice(sampleRequestObject1)
-  t.equal(totalCost1, 360)
+  t.equal(totalCost1, 360, "should equal 360")
   t.end()
 })
 
 tape("Calculates the total cost with discount", (t) => {
   const totalCost2 = calcTotalPrice(sampleRequestObject2)
   const totalCost3 = calcTotalPrice(sampleRequestObject3)
-  t.equal(totalCost2, 300)
-  t.equal(totalCost3, 500)
+  t.equal(totalCost2, 300, "should equal 300")
+  t.equal(totalCost3, 500, "should equal 500")
   t.end()
 })
