d96667f0dba3f95881be51f5687098570ab5e30b
Balou9 <emmanuel.owosekun@gmail.com>
2021-09-06 07:28:25 +0200

    docs: edits description

--- extermitators-api/test.js
+++ extermitators-api/test.js
@@ -4,16 +4,16 @@ const sampleRequestObject1 = [ "001", "002", "001", "004", "003"]
 const sampleRequestObject2 = [ "002", "002", "001", "004", "003"]
 const sampleRequestObject3 = [ "001", "002", "001", "001", "002", "001", "002"]
 
-tape("Calculates the total cost", (t) => {
-  const totalCost1 = calcTotalPrice(sampleRequestObject1)
-  t.equal(totalCost1, 360, "should equal 360")
+tape("Calculates the total price", (t) => {
+  const totalPrice1 = calcTotalPrice(sampleRequestObject1)
+  t.equal(totalPrice1, 360, "should equal 360")
   t.end()
 })
 
-tape("Calculates the total cost with discount", (t) => {
-  const totalCost2 = calcTotalPrice(sampleRequestObject2)
-  const totalCost3 = calcTotalPrice(sampleRequestObject3)
-  t.equal(totalCost2, 300, "should equal 300")
-  t.equal(totalCost3, 500, "should equal 500")
+tape("Calculates the total price with discount", (t) => {
+  const totalPrice2 = calcTotalPrice(sampleRequestObject2)
+  const totalPrice3 = calcTotalPrice(sampleRequestObject3)
+  t.equal(totalPrice2, 300, "should equal 300")
+  t.equal(totalPrice3, 500, "should equal 500")
   t.end()
 })
